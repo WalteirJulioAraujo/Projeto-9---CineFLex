4d42e431d7db0063ec50e01335ae840ca7fabb6f
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-05-14 20:08:17 -0300

    Projeto finalizado

--- src/components/OrderSucess.js
+++ src/components/OrderSucess.js
@@ -5,15 +5,21 @@ export default function OrderSucess({info}){
         <div className="title">Pedido feito com sucesso</div>
         <div>
             <div className="subtitle">Filme e sessão</div>
-            <div>{`${info.title}\n${info.date} ${info.hour}`}</div>
+            <div className="info">
+                <div>{`${info.title}`}</div>
+                <div>{`${info.date} ${info.hour}`}</div>
+            </div> 
         </div>
         <div>
             <div className="subtitle">Ingressos</div>
-            {info.seats.map((e)=><div>{`Assento: ${e}`}</div>)}
+            {info.seats.map((e)=><div className="info">{`Assento: ${e}`}</div>)}
         </div>
         <div>
             <div className="subtitle">Comprador</div>
-            <div>{`Nome:${info.name} - CPF:${info.cpf}`}</div>
+            <div className="info">
+                <div>{`Nome: ${info.name}`}</div>
+                <div>{`CPF: ${info.cpf}`}</div>
+            </div>
         </div>
         </>
     )
