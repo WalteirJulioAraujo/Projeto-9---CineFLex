df7f6c6a82aa03c71e1a0b5d2762fe98965a9e9a
thiagoribb <thiribeiro142@gmail.com>
2021-05-18 17:20:56 -0300

    Preparando review

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import ReactDom from "react-dom"
-import App from "./components/App"
-import "./styles/reset.css"
-import "./styles/styles.css"
-
-ReactDom.render(<App />,document.querySelector(".root"))
